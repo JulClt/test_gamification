a51c59f414eb017b3151dd2c36af71ce0dc7f63a
Julien Collet <julien.collet01@gmail.com>
2014-04-22 18:35:14 +0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestGamification::Application.config.secret_token = 'fd1552f1c98d546ff8dfaca7f53a5ea8cb636ec3305404b90b7b561c4df545c3b428c5c6d8ff125a451c79d58e0e9a06012d0920f09567cd2473fddeca2a0fd3'
