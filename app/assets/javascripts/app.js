f8b62b4288a5bd82384a6d01713e4861df0d289e
Julien Collet <julien.collet01@gmail.com>
2014-04-24 18:31:41 +0200

    Add template for form

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,19 +1,51 @@
 angular.module('plunker', ['ui.bootstrap']);
-function AssistantCtrl($scope) {
+function AssistantCtrl($scope, $rootScope) {
   $scope.oneAtATime = true;
-
-  $scope.groups = [
+  /*
+  status = 1 : TO DO
+  status = 2 : IN PROGRESS
+  status = 3 : DONE
+  */
+  $rootScope.groups = [
     {
       title: "Coordonnées",
       preview: "ClicRDV, 9 Rue Maurice Mallet, 92521 Issy les Mouline...",
-      content: "test.html"
+      content: "http://localhost:3000/assets/partials/coordonnees-template.html",
+      status: 3
     },
     {
       title: "Secteur d'activité",
       preview: "Docteur généraliste",
-      content: "partials/coordonnees-template.html"
+      content: "http://localhost:3000/assets/partials/secteur-activite-template.html",
+      status: 3
+    },
+    {
+      title: "Prise de rendez-vous par Internet",
+      preview: "Activée",
+      content: "http://localhost:3000/assets/partials/rendez-vous-internet-template.html",
+      status: 3
+    },
+    {
+      title: "Horaires",
+      preview: "",
+      content: "http://localhost:3000/assets/partials/horaire-template.html",
+      status: 1
+    },
+    {
+      title: "Agendas",
+      preview: "",
+      content: "http://localhost:3000/assets/partials/agenda-template.html",
+      status: 1
+    },
+    {
+      title: "RDV proposés",
+      preview: "",
+      content: "http://localhost:3000/assets/partials/rendez-vous-proproses-template.html",
+      status: 1
     }
   ];
+
+  $rootScope.groupNb = $scope.groups.length;
 
   $scope.items = ['Item 1', 'Item 2', 'Item 3'];
 
